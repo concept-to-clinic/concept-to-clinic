20bfe6d22c9320de528ceee320636b57662e15ad
Willi Gierke <willi.gierke@student.hpi.de>
2017-08-26 10:19:09 +0200

    use APP_VERSION_NUMBER in settings

--- interface/backend/static/views.py
+++ interface/backend/static/views.py
@@ -1,7 +1,6 @@
+from django.conf import settings
 from django.shortcuts import render
 
 
 def home(request):
-    current_hash = open('/HEAD', 'r').readlines()[-1].split(' ')[1]
-    short_hash = current_hash[:7]
-    return render(request, '../frontend/index.html', {'short_hash': short_hash})
+    return render(request, '../frontend/index.html', {'short_hash': settings.APP_VERSION_NUMBER})
